ceb468ddea3891e36cee784d27d09d61bca6ebb5
Ahmed Khalafallah <52683244+AhmedKhalafallah1999@users.noreply.github.com>
2022-11-27 07:34:41 +0200

    Time: 3 ms (81.20%), Space: 7.2 MB (5.09%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        set<char>st;
-        for (int i=0 ; i<sentence.size() ; i++){
-            st.insert(sentence[i]);
-        }
+        unordered_set<char>st(sentence.begin(),sentence.end());
         if (st.size()==26) return true;
         return false;
     }
